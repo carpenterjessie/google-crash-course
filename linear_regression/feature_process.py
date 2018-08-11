6fc84135d404a46d3cee1459e6f7cd0efdb13e11
jessie <tanglijessie@126.com>
2018-08-11 14:57:48 +0800

    add churn example
    add churn example

--- linear_regression/feature_process.py
+++ linear_regression/feature_process.py
@@ -14,7 +14,7 @@ tf.logging.set_verbosity(tf.logging.ERROR)
 pd.options.display.max_rows=10
 pd.options.display.float_format = '{:.1f}'.format
 
-california_housing_dataframe = pd.read_csv("/Users/cylg/Documents/GitHub/google-crash-course/first step/data/california_housing_train.csv", sep=",")
+california_housing_dataframe = pd.read_csv("/Users/cylg/Documents/GitHub/google-crash-course/linear_regression/data/california_housing_train.csv", sep=",")
 
 california_housing_dataframe = california_housing_dataframe.reindex(
     np.random.permutation(california_housing_dataframe.index))
